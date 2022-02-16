aa1bcc8fa754ae0553e30b37a1a836e626b9328a
shimo97 <simobolla97@gmail.com>
2022-02-16 23:14:24 +0100

    final version

--- lab3/src/ControlBlock/tb_control.vhd
+++ lab3/src/ControlBlock/tb_control.vhd
@@ -9,15 +9,15 @@ architecture beh of tb_control is
 
 	COMPONENT ControlBlock IS
 		PORT (opcode : IN std_logic_vector(6 downto 0);
-				funct : IN std_logic_vector(2 downto 0);
-				Branch, MemRead, MemtoReg : OUT std_logic;
-				ALUOp : OUT std_logic_vector(3 downto 0);
-				MemWrite, ALUsrc1, ALUsrc2, RegWrite, RegWriteSrc : OUT std_logic);
+			funct : IN std_logic_vector(2 downto 0);
+			Op2ALUbypass, Branch, MemRead, MemtoReg : OUT std_logic;
+			ALUOp : OUT std_logic_vector(3 downto 0);
+			MemWrite, ALUsrc1, ALUsrc2, RegWrite, RegWriteSrc : OUT std_logic);
 	END COMPONENT;
 	
 	signal opcode : std_logic_vector(6 downto 0);
 	signal funct : std_logic_vector(2 downto 0);
-	signal branch, memRead, memtoReg, memWrite, ALUsrc1, ALUsrc2, regWrite, regWriteSrc : std_logic;
+	signal op2ALUbypass, branch, memRead, memtoReg, memWrite, ALUsrc1, ALUsrc2, regWrite, regWriteSrc : std_logic;
 	signal ALUop : std_logic_vector(3 downto 0);
 	
 BEGIN
@@ -57,6 +57,6 @@ BEGIN
 	end process;
 
 	-- DUT
-	DUT : ControlBlock port map (opcode, funct, branch, memRead, memtoReg, ALUop, memWrite, ALUsrc1, ALUsrc2, regWrite, regWriteSrc);
+	DUT : ControlBlock port map (opcode, funct, op2ALUbypass, branch, memRead, memtoReg, ALUop, memWrite, ALUsrc1, ALUsrc2, regWrite, regWriteSrc);
 
 END beh;
